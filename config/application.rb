ea86e4f54f1ebf382ab3380676d73a09100fff8d
arakakitatsuki <20yuteo@gmail.com>
2020-05-18 19:20:01 +0900

    Initialize repository

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RspecApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       helper_specs: false,
+                       routing_specs: false,
+                       view_specs: false,
+                       controller_specs: false
+    end
+  end
+end
