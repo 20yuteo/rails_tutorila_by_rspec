b595a0f4e24c77fd2d8916cc45183c58e9d2469f
arakakitatsuki <20yuteo@gmail.com>
2020-05-18 20:45:29 +0900

    Finish static pages

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-
-  describe "GET /home" do
-    it "returns http success" do
-      get "/static_pages/home"
-      expect(response).to have_http_status(:success)
+    context 'GET #home' do
+        before { get root_path }
+        it 'responds successfully' do
+            expect(response).to have_http_status 200
+        end
+        it "has title 'Ruby on Rails Tutorial Sample App'" do
+            expect(response.body).to include 'Ruby on Rails Tutorial Sample App'
+            expect(response.body).to_not include '| Ruby on Rails Tutorial Sample App'
+        end
     end
-  end
 
-  describe "GET /help" do
-    it "returns http success" do
-      get "/static_pages/help"
-      expect(response).to have_http_status(:success)
+    context 'GET #help' do
+        before { get static_pages_help_url }
+        it 'responds successfully' do
+            expect(response).to have_http_status 200
+        end
+        it "has title 'Home | Ruby on Rails Tutorial Sample App'" do
+            expect(response.body).to include 'Help | Ruby on Rails Tutorial Sample App'
+        end
     end
-  end
 
+    context 'GET #about' do
+        before { get static_pages_about_url }
+        it 'responds succesfully' do
+            expect(response).to have_http_status 200
+        end
+        it "has title 'Home | Ruby on Rails Tutorial Sample App'" do
+            expect(response.body).to include 'About | Ruby on Rails Tutorial Sample App'
+        end
+    end
 end
