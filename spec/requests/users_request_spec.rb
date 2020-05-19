2b807ea0fdd39bde1abefc17c7fdf205f43b07b9
arakakitatsuki <20yuteo@gmail.com>
2020-05-19 23:47:36 +0900

    Finish user signup

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -9,4 +9,37 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  describe "POST #create" do
+
+    context 'valid request' do
+      it 'adds a user' do
+        expect do
+          post signup_path, params: { user: attributes_for(:user) }
+        end.to change(User, :count).by(1)
+      end
+    
+    context 'add a user' do
+        before { post signup_path, params: { user: attributes_for(:user) } }
+        subject { response }
+        it { is_expected.to redirect_to user_path(User.last) }
+        it { is_expected.to have_http_status 302 }
+    end
+  end
+
+    context 'invalid request' do
+      let(:user_params) do
+        attributes_for(:user, name: '',
+                              email: 'user@invalid',
+                              password: '',
+                              password_confirmation: '')
+    end
+
+    it 'dose not add a user' do
+      expect do
+        post signup_path, params: { user: user_params }
+      end.to change(User, :count).by(0)
+    end
+  end
 end
+end
+
