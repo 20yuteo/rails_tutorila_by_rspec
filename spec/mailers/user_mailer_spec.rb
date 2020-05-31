617e6147d96d8aa4c58ee4979ab54ee6214d1a03
arakakitatsuki <20yuteo@gmail.com>
2020-05-31 15:36:38 +0900

    Add password reset

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -20,4 +20,21 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail.body.encoded).to match CGI.escape(user.email)
     end
   end
+
+  describe "password_reset" do
+    let(:mail) { UserMailer.password_reset(user) }
+
+    it "renders the headers" do
+      user.reset_token = User.new_token
+      expect(mail.to).to eq ["user_5@example.com"]
+      expect(mail.from).to eq ["noreply@example.com"]
+      expect(mail.subject).to eq "Password reset"
+    end
+
+    it "renders the body" do
+      user.reset_token = User.new_token
+      expect(mail.body.encoded).to match user.reset_token
+      expect(mail.body.encoded).to match CGI.escape(user.email)
+    end
+  end
 end
